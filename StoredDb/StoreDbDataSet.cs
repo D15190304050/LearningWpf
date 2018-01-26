0216d55a1c68c8b9c9d181ec29fd65eb86f2133c
鲍方舟 <shakuganno00@163.com>
2018-01-26 21:29:52 +0800

    Last updated : 2018.01.26 21:29

---
+++ StoredDb/StoreDbDataSet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace StoredDb
+{
+    public class StoreDbDataSet
+    {
+        public DataTable GetProducts()
+        {
+            return ReadDataSet().Tables[0];
+        }
+
+        public DataSet GetCategoriesAndProducts()
+        {
+            return ReadDataSet();
+        }
+
+        internal static DataSet ReadDataSet()
+        {
+            DataSet ds = new DataSet();
+            ds.ReadXmlSchema("store.xsd");
+            ds.ReadXml("store.xml");
+            return ds;
+        }
+    }
+}
--- StoredDb/StoredDb.cs
+++ StoredDb/StoredDb.cs
@@ -3,11 +3,89 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
 
 namespace StoredDb
 {
     public class StoredDb
     {
+        public Product GetProduct(int ID)
+        {
+            DataSet ds = StoreDbDataSet.ReadDataSet();
+            DataRow productRow = ds.Tables["Products"].Select("ProductID = " + ID.ToString())[0];
+            Product product = new Product((string)productRow["ModelNumber"],
+                                          (string)productRow["ModelName"],
+                                          (decimal)productRow["UnitCost"],
+                                          (string)productRow["Description"],
+                                          (int)productRow["CategoryID"],
+                                          (string)productRow["CategoryName"],
+                                          (string)productRow["ProductImage"]);
+            return product;
+        }
 
+        public ICollection<Product> GetProducts()
+        {
+            DataSet ds = StoreDbDataSet.ReadDataSet();
+
+            ObservableCollection<Product> products = new ObservableCollection<Product>();
+            foreach (DataRow productRow in ds.Tables["Products"].Rows)
+            {
+                products.Add(new Product((string)productRow["ModelNumber"],
+                                         (string)productRow["ProductName"],
+                                         (decimal)productRow["UnitCost"],
+                                         (string)productRow["Description"],
+                                         (int)productRow["CategoryID"],
+                                         (string)productRow["CategoryName"],
+                                         (string)productRow["ProductImage"]));
+            }
+            return products;
+        }
+
+        public ICollection<Category> GetCategoriesAndProducts()
+        {
+            DataSet ds = StoreDbDataSet.ReadDataSet();
+            DataRelation relCategoryProduct = ds.Relations[0];
+
+            ObservableCollection<Category> categories = new ObservableCollection<Category>();
+            foreach (DataRow categoryRow in ds.Tables["Categories"].Rows)
+            {
+                ObservableCollection<Product> products = new ObservableCollection<Product>();
+                foreach (DataRow productRow in categoryRow.GetChildRows(relCategoryProduct))
+                {
+                    products.Add(new Product((string)productRow["ModelNumber"],
+                                             (string)productRow["ModelName"],
+                                             (decimal)productRow["UnitCost"],
+                                             (string)productRow["Description"]));
+                }
+                categories.Add(new Category(categoryRow["CategoryName"].ToString(), products));
+            }
+            return categories;
+        }
+
+        public ICollection<Product> GetProductsFillteredWithLinq(decimal minCost)
+        {
+            // Get the full list of products.
+            ICollection<Product> products = GetProducts();
+
+            // Create a second collection with matching products.
+            IEnumerable<Product> matches =
+                from product in products
+                where product.UnitCost >= minCost
+                select product;
+            return new ObservableCollection<Product>(matches.ToList());
+        }
+
+        public ICollection<Category> GetCategories()
+        {
+            DataSet ds = StoreDbDataSet.ReadDataSet();
+
+            ObservableCollection<Category> categories = new ObservableCollection<Category>();
+            foreach (DataRow categoryRow in ds.Tables["Categories"].Rows)
+                categories.Add(new Category((string)categoryRow["CategoryName"], (int)categoryRow["CategoryID"]));
+            return categories;
+        }
     }
 }

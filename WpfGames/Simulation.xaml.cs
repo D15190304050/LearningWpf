67ac360b32d4c8e81707b3cbb41a394859d11335
鲍方舟 <shakuganno00@163.com>
2018-02-22 00:27:09 +0800

    Last updated: 2018.02.22 00:27

---
+++ WpfGames/Simulation.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfGames
+{
+    /// <summary>
+    /// Simulation.xaml 的交互逻辑
+    /// </summary>
+    public partial class Simulation : Window
+    {
+        public Simulation()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}

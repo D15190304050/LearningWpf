dd95854183e6b46d60a2e31d006e6aff95ce1da3
鲍方舟 <shakuganno00@163.com>
2018-05-09 11:02:41 +0800

    Created a new project named "MusicPlayer".

---
+++ MusicPlayer/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+
+namespace MusicPlayer
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private LinkedList<Song> songs;
+        private MediaPlayer musicPlayer;
+
+        public MainWindow()
+        {
+            songs = new LinkedList<Song>();
+            musicPlayer = new MediaPlayer();
+
+            InitializeComponent();
+        }
+
+        private void cmdAddMusic_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                String fullFilePath = openFileDialog.FileName;
+                songs.AddLast(new Song(fullFilePath));
+                lstSongs.Items.Refresh();
+            }
+        }
+
+        private void cmdPlay_Click(object sender, RoutedEventArgs e)
+        {
+            Song selectedSong = lstSongs.SelectedItem as Song;
+            if (selectedSong != null)
+            {
+                musicPlayer.Open(new Uri(selectedSong.FullFilePath, UriKind.Absolute));
+                musicPlayer.Play();
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            lstSongs.ItemsSource = songs;
+        }
+    }
+}
---
+++ MusicPlayer/Song.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Media;
+
+namespace MusicPlayer
+{
+    public class Song
+    {
+        public string FullFilePath { get; }
+        public string FileName { get; }
+
+        public string Time { get; }
+
+        public Song(string fullFilePath)
+        {
+            this.FullFilePath = fullFilePath;
+            FileInfo file = new FileInfo(fullFilePath);
+            this.FileName = file.Name;
+            MediaPlayer mp = new MediaPlayer();
+            mp.Open(new Uri(fullFilePath, UriKind.Absolute));
+            Time = mp.NaturalDuration.ToString();
+        }
+    }
+}

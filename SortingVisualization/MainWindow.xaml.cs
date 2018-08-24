d64ff6b84c190f54d0ca8fdf3317e09ddf7852a6
鲍方舟 <shakuganno00@163.com>
2018-08-24 21:59:12 +0800

    Last updated: 2018.08.24 21:59

---
+++ SortingVisualization/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Reflection;
+
+namespace SortingVisualization
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            Button cmd = (Button) e.OriginalSource;
+            string windowName = cmd.Content.ToString().Replace(" ", "");
+
+            Type type = this.GetType();
+            Assembly assembly = type.Assembly;
+            Window window = (Window)assembly.CreateInstance(type.Namespace + "." + windowName);
+
+            window?.ShowDialog();
+        }
+    }
+}

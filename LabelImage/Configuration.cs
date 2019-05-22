7577237bca4a571d8bf07d4878263515dd55bf5e
鲍方舟 <shakuganno00@163.com>
2019-05-22 21:55:37 +0800

    Start to develpo LabelImage using C# and WPF.

---
+++ LabelImage/Configuration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabelImage
+{
+    public class Configuration
+    {
+        private int uiFontSize;
+
+        public int UIFontSize
+        {
+            get { return uiFontSize; }
+            set { uiFontSize = value; }
+        }
+
+        public Configuration()
+        {
+            uiFontSize = 16;
+        }
+    }
+}
---
+++ LabelImage/ZoomInCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace LabelImage
+{
+    public class ZoomInCommand : ICommand
+    {
+        private const int MaxZoomIn = 3;
+        private int currentZoomIn;
+
+        public ZoomInCommand()
+        {
+            currentZoomIn = 1;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return currentZoomIn >= MaxZoomIn;
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
